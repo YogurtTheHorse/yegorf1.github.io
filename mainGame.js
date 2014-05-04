408e96e23477eed1437656649b308c68cfe3c731
yegorf1 <yegorf1@gmail.com>
2014-05-04 14:06:33 +0400

    New physics!

--- mainGame.js
+++ mainGame.js
@@ -30,12 +30,16 @@ var PLAYERS_COLORS = [
 		]
 	];
 //Game variables
-var points = [0, 0, 0, 0],
+var chips,
+	game,
+	points = [0, 0, 0, 0],
+	chipsCount = [5, 5, 5, 5],
 	texts = [null, null, null, null],
 	playersCount = 4,
 	frame = 0,
 	turn = 0,
-	somethingMoving = false;
+	somethingMoving = false,
+	chipsCollisionGroup;
 
 //State
 mainGameState = function (game) { };
@@ -48,30 +52,33 @@ mainGameState.prototype = {
 }
 
 function MainGamePreload () {
-	this.game.stage.backgroundColor = '#ddd';
-	this.game.load.image('chip', 'assets/chip.png');
+	game = this.game;
+	game.stage.backgroundColor = '#ddd';
+	game.load.image('chip', 'assets/chip.png');
 }
 
 function MainGameCreate () {
-    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+	game.physics.startSystem(Phaser.Physics.P2JS);
+	game.physics.p2.restitution = 0.8;
 
-    //Draw line in the center
-	bmd = this.game.add.bitmapData(WINDOW_WIDTH, WINDOW_HEIGHT);
-        
-    bmd.ctx.beginPath();
-    bmd.ctx.lineWidth = "2";
-    bmd.ctx.strokeStyle = 'white';
-    bmd.ctx.setLineDash([5, 6]);
-    bmd.ctx.moveTo(0, convertY(0.5));
-    bmd.ctx.lineTo(convertX(1), convertY(0.5));
-    bmd.ctx.stroke();
-    bmd.ctx.closePath();
-    this.game.add.sprite(0, 0, bmd); 
+	//Draw line in the center
+	bmd = game.add.bitmapData(WINDOW_WIDTH, WINDOW_HEIGHT);
 
+	bmd.ctx.beginPath();
+	bmd.ctx.lineWidth = "2";
+	bmd.ctx.strokeStyle = 'white';
+	bmd.ctx.setLineDash([5, 6]);
+	bmd.ctx.moveTo(0, convertY(0.5));
+	bmd.ctx.lineTo(convertX(1), convertY(0.5));
+	bmd.ctx.stroke();
+	bmd.ctx.closePath();
+	game.add.sprite(0, 0, bmd); 
+	this.shape = game.add.graphics(0, 0);
 
-	this.shape = this.game.add.graphics(0, 0);
-	this.chips = this.game.add.group();
-	this.chips.enableBody = true;
+	chipsCollisionGroup = game.physics.p2.createCollisionGroup();
+	chips = game.add.group();
+	chips.enableBody = true;
+	chips.physicsBodyType = Phaser.Physics.P2JS;
 
 	setupFirstPlayer(this);
 	setupSecondPlayer(this);
@@ -81,6 +88,7 @@ function MainGameCreate () {
 			setupFourthPlayer(this);
 		}
 	}
+	chips.setAll('static', true);
 }
 
 function setupFirstPlayer(state) {
@@ -91,92 +99,40 @@ function setupFirstPlayer(state) {
 		rectLenght /= 2;
 	state.shape.drawRect(0, convertY(1 - SAVE_ZONE_SIZE), rectLenght, convertY(SAVE_ZONE_SIZE));
 
-	for (var i = 0; i < 5; i++) {
-		var posX = convertX(i / 5 + 1 / 10);
-		if (playersCount > 2) {
-			posX /= 2;
-		}
-		setupChip(
-					state.chips.create(posX, convertY(1 - SAVE_ZONE_SIZE / 2), 'chip'), 
-					i, PLAYER_FIRST
-				 );
-	}
-	//TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
-	//TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
-	//TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
-	//TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
-	//TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
-	//TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
-	//TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
-	//TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
-	//TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
-	//TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
-	//TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
-	//TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
-	//TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
-	//TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
-	//TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO
-	texts[PLAYER_FIRST] = state.game.add.text(
-												3, convertY(0.5) - 10, 
-												"0", 
-												{
-													font: "16px Arial",
-													fill: "#0"
-												}
-											 );
-	texts[PLAYER_FIRST].anchor.setTo(0.5, 0.5);
+	var chip = chips.create(convertX(0.5), convertY(0.5), 'chip');
+	setupChip(chip, PLAYER_FIRST, game);
+	chip.moveTo =  	{
+						x: rectLenght / 2,
+						y: convertY(1 - SAVE_ZONE_SIZE / 2)
+					};
 }
 
 function setupSecondPlayer(state) {
 	state.shape.lineStyle(2, PLAYERS_COLORS[PLAYER_SECOND], 1);
 	state.shape.beginFill(PLAYERS_COLORS[PLAYER_SECOND], 1);
 	var rectLenght = convertX(1);
-	if (playersCount > 2)
+	if (playersCount > 3)
 		rectLenght /= 2;
 	state.shape.drawRect(0, 0, rectLenght, convertY(SAVE_ZONE_SIZE));
-
-	for (var i = 0; i < 5; i++) {
-		var posX = convertX(i / 5 + 1 / 10);
-		if (playersCount > 2) {
-			posX /= 2;
-		}
-		setupChip	(
-			state.chips.create(posX, convertY(SAVE_ZONE_SIZE / 2), 'chip'), 
-			i, PLAYER_SECOND
-		);
-	}
 }
 
 function setupThirdPlayer(state) {
 	state.shape.lineStyle(2, PLAYERS_COLORS[PLAYER_THIRD], 1);
 	state.shape.beginFill(PLAYERS_COLORS[PLAYER_THIRD], 1);
-
 	state.shape.drawRect(convertX(0.5), convertY(1 - SAVE_ZONE_SIZE), convertX(1), convertY(SAVE_ZONE_SIZE));
-
-	for (var i = 0; i < 5; i++) {
-		var posX = convertX(0.5) + convertX(i / 5 + 1 / 10) / 2;
-		setupChip(
-					state.chips.create(posX, convertY(1 - SAVE_ZONE_SIZE / 2), 'chip'), 
-					i, PLAYER_THIRD
-				 );
-	}
 }
 
 function setupFourthPlayer(state) {
 	state.shape.lineStyle(2, PLAYERS_COLORS[PLAYER_FOURTH], 1);
 	state.shape.beginFill(PLAYERS_COLORS[PLAYER_FOURTH], 1);
 	state.shape.drawRect(convertX(0.5), 0, convertX(0.5), convertY(SAVE_ZONE_SIZE));
-
-	for (var i = 0; i < 5; i++) {
-		var posX = convertX(0.5) + convertX(i / 5 + 1 / 10) / 2;
-		setupChip	(
-			state.chips.create(posX, convertY(SAVE_ZONE_SIZE / 2), 'chip'), 
-			i, PLAYER_FOURTH
-		);
-	}
 }
 
-function setupChip(chip, number, owner) {
+function setupChip(chip, owner, game) {
+	if (chipsCount[owner] < 1)
+		return false;
+	chipsCount[owner]--;
+
 	var MAX_MASS = 6;
 	var mass = Math.ceil(Math.random() * (MAX_MASS - 1)) + 1;
 
@@ -184,30 +140,27 @@ function setupChip(chip, number, owner) {
 	chip.mass = mass * 100;
 	chip.anchor.setTo(0.5, 0.5);
 	chip.tint = PLAYERS_COLORS[owner];
-	chip.number = number;
 	chip.owner = owner;
 	chip.dragging = false;
+	chip.body.collides(chipsCollisionGroup);
+	chip.body.setCircle(chip.width / 2);
 	chip.inputEnabled = true;
 	chip.input.start(0, true);
-	chip.body.collideWorldBounds = true;
-	chip.body.allowGravity = false;
-	chip.body.bounce.setTo(0.7, 0.7);
-	chip.body.immovable = true;
 	chip.events.onInputDown.add(drag);
 	chip.events.onInputUp.add(stopDrag);
+
+	return true;
 }
 
 function drag(chip, pointer) {
-	if (turn != chip.owner)
+	if (turn != chip.owner || chip.moveTo)
 		return;
 
 	if (isInBounds(BOUNDS[chip.owner], chip.y) && !chip.dragging){
-	    chip.body.moves = false;
-	    chip.dragging = true;
-	    chip.pointer = pointer;
-	    chip.oldPos = { x: chip.x, y: chip.y };
-	    chip.startPos = chip.oldPos;
-	    chip.body.immovable = false;
+		chip.dragging = true;
+		chip.pointer = pointer;
+		chip.oldPos = { x: chip.x, y: chip.y };
+		chip.startPos = chip.oldPos;
 	}
 }
 
@@ -217,7 +170,6 @@ function stopDrag (chip, pointer) {
 			if (chip.startPos) {
 				chip.moveTo = chip.startPos;
 			}
-			chip.body.moves = true;;
 			chip.dragging = false;
 			chip.pointer = null
 			chip.oldPos = null;
@@ -225,53 +177,95 @@ function stopDrag (chip, pointer) {
 			var Xvector = (chip.x - chip.oldPos.x) * 10,
 				Yvector = (chip.y - chip.oldPos.y) * 10;
 
-			chip.body.moves = true;;
-			chip.body.velocity.setTo(Xvector, Yvector);
+			chip.body.velocity.x = Xvector;
+			chip.body.velocity.y = Yvector;
 
 			chip.dragging = false;
 			chip.pointer = null
 			chip.oldPos = null;
 
-			turn = (turn + 1) % playersCount;
+			if (!nextTurn()) {
+				alert('game over');
+			}
 		}
 	}
 }
 
+function nextTurn () {
+	var i = 1;
+	while (!chipsCount[(turn + i) % playersCount]) {
+		i++;
+		if (i >= 5)
+			return false;
+	}
+
+	turn = (turn + i) % playersCount;
+
+	var realChipY = SAVE_ZONE_SIZE / 2,
+		chipX = 0.5;
+
+	if (turn == PLAYER_FIRST || turn == PLAYER_THIRD) {
+		realChipY = 1 - realChipY;
+	}
+
+	if (playersCount > 2) {
+		if (turn == )
+		chipX /= 2;
+	}
+	
+	if (turn > PLAYER_SECOND) {
+		chipX += 0.5;
+	}
+
+	realChipY = convertY(realChipY);
+	chipX = convertX(chipX);
+
+	var chipY = -realChipY;
+
+	if (turn == PLAYER_FIRST || turn == PLAYER_THIRD) {
+		chipY = WINDOW_HEIGHT - chipY;
+	}
+
+	var chip = chips.create(chipX, chipY, 'chip');
+	setupChip(chip, turn, game);
+	chip.moveTo =  	{
+						x: chipX,
+						y: realChipY
+					};
+
+	return true;
+}
+
 function MainGameUpdate () {
-	this.game.physics.arcade.collide(this.chips);
-	this.chips.forEach(function (chip) {
+	chips.forEach(function (chip) {
 		if (chip.moveTo) {
-			var k = 0.05,
+			var k = 0.2,
 				dist = getSqrDistance(chip.moveTo, chip);
 			if (dist < 3) {
 				chip.x = chip.moveTo.x;
 				chip.y = chip.moveTo.y;
 				chip.moveTo = null;
-				chip.body.velocity.setTo(0, 0);
-	    		chip.body.immovable = true;
+				chip.body.velocity.x = 0;
+				chip.body.velocity.y = 0;
 			} else if (dist < 15) {
 				k /= 10;
 			} else {
-				chip.body.velocity.setTo((chip.moveTo.x - chip.x) / k, (chip.moveTo.y - chip.y) / k);
+				chip.body.velocity.x = (chip.moveTo.x - chip.x) / k
+				chip.body.velocity.y = (chip.moveTo.y - chip.y) / k;
 			}
 		} else if (chip.dragging && chip.pointer) {
 			if (isInBounds(BOUNDS[chip.owner], chip.y)) {
 				if (frame % 5 == 0) {
-		    		chip.oldPos = { x: chip.x, y: chip.y };
+					chip.oldPos = { x: chip.x, y: chip.y };
 				}
-				chip.x = chip.pointer.worldX;
-				chip.y = chip.pointer.worldY;
+				chip.body.velocity.x = 10 * (chip.pointer.worldX - chip.x);
+				chip.body.velocity.y = 10 * (chip.pointer.worldY - chip.y);
 			} else {
 				stopDrag(chip, chip.pointer);
 			}
-		} else if (chip.body.isRunning) {
-	    	chip.body.immovable = true;
 		} else {
-			chip.body.velocity.setTo(
-										chip.body.velocity.x * 0.9995, 
-										chip.body.velocity.y * 0.9995
-									);
-
+			/*chip.body.velocity.x = chip.body.velocity.x * 0.9995;
+			chip.body.velocity.y = chip.body.velocity.y * 0.9995;*/
 		}
 	});
 	frame++;
